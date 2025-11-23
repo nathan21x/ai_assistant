06fe171646259802d943011b25013fbfc7729f2b
Jonathan <jonathan.legarde@gmail.com>
2025-11-23 09:44:05 +0800

    - wrong server file for sending email endpoint

--- api/server.js
+++ api/server.js
@@ -28,6 +28,38 @@ const sessionHistory = new Map();
 app.use(express.json());
 app.use(cors());
 
+app.post("/api/send_email", async (req, res) => {
+    try {
+        const { to, message } = req.body;
+
+        try {
+            const transporter = nodemailer.createTransport({
+                service: "gmail",
+                auth: {
+                    user: 'pangloginlangtlga@gmail.com',
+                    pass: 'xohn ceob ftys lvkg'
+                }
+            });
+            try {
+                await transporter.sendMail({
+                    from: 'pangloginlangtlga@gmail.com',
+                    to,
+                    subject: "Message from Vite App",
+                    html: message
+                });
+            } catch (ex) {
+                console.log('error ', ex)
+            }
+        } catch (ex) {
+            console.log("error 1 ", ex)
+        }
+
+        res.json({ success: true });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+})
+
 app.post('/api/ask_api', async (req, res) => {
     const { user_id, question, config } = req.body;
 
